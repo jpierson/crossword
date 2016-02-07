653bb6553e813ed23daff34d3d49b2af2cef8b71
Jeff <jeffrey.pierson@gmail.com>
2016-02-07 00:14:40 -0500

    Implemented add word capability using component state for now.

--- Hello.jsx
+++ Hello.jsx
@@ -2,7 +2,44 @@
 var React = require('react')
 module.exports = React.createClass({
     displayName: 'HelloReact',
+    
+    getInitialState: function() {
+        // naming it initialX clearly indicates that the only purpose
+        // of the passed down prop is to initialize something internally
+        return {
+            words: [],
+            
+        };
+    },
+    
+    addWord: function() {
+        this.setState({ words: this.state.words.concat([""]) });
+    },
+    
+    removeWord: function(word) {
+        console.log(`Removing word ${word}`);
+        this.setState({ words: this.state.words.filter(w => w !== word)});
+    },
+    
+    onWordChange: function(i, newWord) {
+        this.setState({ words: 
+            this.state.words
+                .slice(0,i)
+                .concat([newWord])
+                .concat(this.state.words.slice(i+1))});        
+    },
+    
     render: function(){
-        return <div>Hello Crossword!</div>
+        return <div>
+            <ul>
+                {this.state.words && this.state.words.map((word, i) =>
+                    <li key={i}>
+                        <input type="text" onChange={(e) => this.onWordChange(i, e.value)}></input>
+                        <button onClick={() => this.removeWord(word)}>X</button>
+                    </li>
+                )}
+            </ul>
+            <button onClick={() => this.addWord()}>Add word</button>
+        </div>
     }
 })
